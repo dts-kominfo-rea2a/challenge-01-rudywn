47d13edb9d98d8e78e832754910ddf274e4ffcf5
rudywn <rudy.hrm1@gmail.com>
2022-05-29 02:07:53 +0700

    feat: penasaran

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const favoriteColor = {
-    firstUser: new Set(['Yellow', 'Pink', 'White', 'Purple']),
-    secondUser: new Set(['Blue', 'Black', 'Grey']),
-}
-const favoriteRestaurant = {
-    firstUser: new Set(['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']),
-    secondUser: new Set(['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy']),
-}
 
 const firstUser = {
     name: "Monica",
     gender: "Female",
     age: 17,
     email: "monica@dingdong.com",
-    favoriteColor: [...favoriteColor.firstUser],
+    favoriteColor: Array.from(new Set(['Yellow', 'Pink', 'White', 'Purple'])),
     isHavePet: true,
     education: [
         {name: "SD 01", city: "Jakarta", graduate: "2016"},
         {name: "SMP 02", city: "Jakarta", graduate: "2019"},
         {name: "SMA 03", city: "Tangerang"}
     ],
-    favoriteRestaurant: [...favoriteRestaurant.firstUser]
+    favoriteRestaurant: Array.from(new Set(['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']))
 };
 
 const secondUser = {
@@ -30,7 +22,7 @@ const secondUser = {
     gender: "Male",
     age: 23,
     email: "wendy@dingdong.com",
-    favoriteColor: [...favoriteColor.secondUser],
+    favoriteColor: Array.from(new Set(['Blue', 'Black', 'Grey'])),
     isHavePet: false,
     education: [
         {name: "SD 02", city: "Jakarta", graduate: "2010"},
@@ -38,7 +30,7 @@ const secondUser = {
         {name: "SMA 01", city: "Surabaya", graduate: "2016"},
         {name: "Universitas Maju", city: "Tangerang"}
     ],
-    favoriteRestaurant: [...favoriteRestaurant.secondUser]
+    favoriteRestaurant: Array.from(new Set(['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy'])),
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
